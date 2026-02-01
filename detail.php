3cc3f7eabc605946bbed84f5f9864b4abc9716da
goojag <rachan.jack@gmail.com>
2026-02-01 13:47:34 +0700

    Detail page

---
+++ detail.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <?php include_once('inc/meta.php'); ?>
+
+  <title>.:: Tabienthai</title>
+
+</head>
+
+<body>
+  <div class="main">
+
+    <?php include_once('inc/header.php'); ?>
+
+    <div class="wrapper">
+
+      <section class="detail-search">
+        <div class="container">
+          <?php include("inc/search.php"); ?>
+        </div>
+      </section>
+
+      <div class="breadcrumbs">
+        <div class="container">
+          <nav>
+            <a href="/">หน้าแรก > </a>
+            <a href="/">ทะเบียนสวย VIP > </a>
+            <span>รายละเอียดทะเบียน 1ขษ 2</span>
+          </nav>
+        </div>
+      </div>
+
+      <section class="detail-tabien">
+        <div class="container">
+          <div class="detail-tabien-content">
+            <div class="contact-top">
+              <a href="/">
+                <img src="images/phone-brown.svg" alt="">
+              </a>
+              <a href="/">
+                <img src="images/line-green.svg" alt="">
+              </a>
+            </div>
+            <div class="left">
+              <div class="tabien-card">
+                <div class="tabien-thumb">
+                  <img src="images/tabien-img.png" alt="">
+                  <div class="tabien-text">
+                    <div class="number">1ขษ 2</div>
+                    <div class="province">กรุงเทพมหานคร</div>
+                  </div>
+                </div>
+                <div class="bottom">
+                  <div class="tabien-price">
+                    <span>ราคา</span>
+                    <strong>590,000 บาท</strong>
+                  </div>
+                  <div class="sum">
+                    ผลรวม
+                    <strong>22</strong>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="right">
+              <h1>1ขษ 2</h1>
+              <h2>590,000 บาท</h2>
+              <h3>หมายเลข 9 ดาวพระเกตุ ถือว่าเป็นธาตุลม ญาณ การหยั่งรู้ ต่างประเทศ
+                มรดก โหราศาสตร์ นักบวช สถานที่ศักดิ์สิทธิ์ คนทรงเจ้า การมีอายุยืน</h3>
+              <table>
+                <tr>
+                  <td>จังหวัด</td>
+                  <td><strong>กรุงเทพมหานคร</strong></td>
+                </tr>
+                <tr>
+                  <td>สถานะ / CODE</td>
+                  <td><strong>มาใหม่ /FA</strong></td>
+                </tr>
+                <tr>
+                  <td>ผลคะแนนรวม</td>
+                  <td><strong>9</strong></td>
+                </tr>
+                <tr>
+                  <td>หมวดหมู่</td>
+                  <td><strong>ทะเบียนสวย VIP</strong></td>
+                </tr>
+                <tr>
+                  <td>หมายเหตุ</td>
+                  <td><strong>ใช้สำหรับ รถเก๋ง , กระบะ 4 ประตู , รถ SUV ,
+                      รถตู้ไม่เกิน 7 ที่นั่งเท่านั้น</strong></td>
+                </tr>
+              </table>
+
+              <nav class="contact-button">
+                <a href="" class="line">
+                  <img src="images/icons/line-icon.svg" alt="">
+                  <span>สนใจจองผ่านทางไลน์</span>
+                </a>
+                <a href="tel:0985555555" class="phone">
+                  <span>โทรเลย 098-555-5555</span>
+                </a>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+
+    </div> <!-- end .wrapper -->
+
+    <?php include_once('inc/footer.php'); ?>
+
+  </div> <!-- end .main -->
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -36,71 +36,7 @@
 
     <div class="home-search">
       <div class="container">
-        <div class="search-form frm">
-          <h2>ค้นหาทะเบียน</h2>
-          <form action="">
-            <div class="form-grid">
-              <div class="column">
-                <input type="text" class="text">
-              </div>
-
-              <div class="column">
-                <div class="select-box">
-                  <select name="" id="">
-                    <option value="">อักษร</option>
-                    <option value="">select</option>
-                    <option value="">select</option>
-                    <option value="">select</option>
-                    <option value="">select</option>
-                  </select>
-                </div>
-              </div>
-              <div>
-                <div class="column">
-                  <div class="select-box">
-                    <select name="" id="">
-                      <option value="">เลขทะเบียน</option>
-                      <option value="">select</option>
-                      <option value="">select</option>
-                      <option value="">select</option>
-                      <option value="">select</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-
-              <div class="column">
-                <div class="select-box">
-                  <select name="" id="">
-                    <option value="">ราคาทั้งหมด</option>
-                    <option value="">select</option>
-                    <option value="">select</option>
-                    <option value="">select</option>
-                    <option value="">select</option>
-                  </select>
-                </div>
-              </div>
-
-              <div class="column">
-                <div class="select-box">
-                  <select name="" id="">
-                    <option value="">ผลรวม</option>
-                    <option value="">select</option>
-                    <option value="">select</option>
-                    <option value="">select</option>
-                    <option value="">select</option>
-                  </select>
-                </div>
-              </div>
-
-              <div class="column">
-                <button class="btn-main" type="submit">
-                  <span>ค้นหาเลย !!</span>
-                </button>
-              </div>
-            </div>
-          </form>
-        </div> <!-- end .search-form -->
+        <?php include("inc/search.php"); ?>
       </div>
     </div>
 
